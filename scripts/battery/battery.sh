ce1a20d0d74831f85f012ea396d4d8f454d1e146
Elliott Landsborough <elliott.landsborough@gmail.com>
2017-09-29 07:23:47 +0100

    Fix battery.sh for desktop

--- scripts/battery/battery.sh
+++ scripts/battery/battery.sh
@@ -1,4 +1,23 @@
-bat0=$(cat /sys/class/power_supply/BAT0/capacity);
-bat1=$(cat /sys/class/power_supply/BAT1/capacity);
-total=$(($bat0 + $bat1));
-echo "$(($total / 2))";
+#!/usr/bin/env bash
+totalCapacity=0;
+batCount=0;
+
+bat=/sys/class/power_supply/BAT0/capacity;
+if [ -f bat ]; then
+	batCap=$(cat ${bat});
+	totalCapacity=$(($totalCapacity + $batCap));
+	batCount=$(($batCount + 1));
+fi
+
+bat=/sys/class/power_supply/BAT1/capacity;
+if [ -f bat ]; then
+	batCap=$(cat ${bat});
+	totalCapacity=$(($totalCapacity + $batCap));
+	batCount=$(($batCount + 1));
+fi
+
+if [ "${batCount}" == "0" ]; then
+	echo 'N/A';
+else
+	echo $(($totalCapacity / $batCount))
+fi
